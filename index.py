aa4672a8cd1406064818f5cacade57cdbf6e006b
HananMustafa <hananmustafaawan@gmail.com>
2024-09-04 22:23:37 +0500

    Added port and ip to listen on

--- index.py
+++ index.py
@@ -1,11 +1,19 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from routes.note import note
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from config.db import conn
 
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(note)
+
+if __name__ == "__main__":
+    # Get the port from the environment variable PORT (default to 8000 if not set)
+    port = int(os.getenv('PORT', 8000))
+    
+    # Start the server with the specified host and port
+    uvicorn.run(app, host="0.0.0.0", port=port)
